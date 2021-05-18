8b91cfb9b248aaa515aa061f70e98c6a0f363950
hadas <hadas6988@gmail.com>
2021-05-18 20:44:32 +0300

    userlogin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import SignUp from "./components/signUp/signUp";
 import Create from "./components/manager/create";
 import Edit from "./components/manager/edit";
 import Manager from "./components/manager/manager";
+import userLogin from "./components/userLogin/userLogin"
+import participant from "./components/participant/participant";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -17,6 +20,8 @@ function App() {
     <Route path="/edit" component={Edit} />
     <Route path="/create" component={Create} />
     <Route path="/manager" component={Manager} />
+    <Route path="/userlogin" component={userLogin} />
+    <Route path="/participant" component={participant} />
   </Switch>
 
     </div>
